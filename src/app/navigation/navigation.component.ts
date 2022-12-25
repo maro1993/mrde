3c6b42347b9e0d5fd82cc847b42786bdb5b1c4f7
maro1993 <markusroettgermann@yahoo.de>
2022-12-25 18:03:54 +0100

    added cv open functionaility

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,27 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { HomeComponent} from '../home/home.component';
 import { Title } from '@angular/platform-browser';
-
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.scss']
+  styleUrls: ['./navigation.component.scss'],
 })
 export class NavigationComponent implements OnInit {
+  constructor(private titleService: Title) {}
 
-
-  constructor(private titleService: Title) { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   public setTitle(newTitle: string) {
     this.titleService.setTitle(newTitle);
   }
 
-
-
-  
-
+  public downloadCv() {
+    window.open('/assets/documents/2021.03.30_Lebenslauf.pdf');
+  }
 }
